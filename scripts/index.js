e88837d035931013369bcbc5447dd0e1bdd84b7f
Artem Dubravtsev <a.dubravtsev@yandex.ru>
2023-03-16 22:04:01 +0300

    Ревью 2

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ const popupElement = document.querySelector('.popup');
 const popupCloseBottonElement = popupElement.querySelector('.popup__button-close');
 const popupOpenBottonElement = document.querySelector('.profile__button-edit');
 const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__form-item_name');
-const jobInput = formElement.querySelector('.popup__form-item_job');
+const nameInput = formElement.querySelector('.popup__form-item_line_name');
+const jobInput = formElement.querySelector('.popup__form-item_line_job');
 const nameTitle = document.querySelector('.profile__title');
 const jobSubtitle = document.querySelector('.profile__subtitle');
 
@@ -16,13 +16,12 @@ const closePopup = function () {
     popupElement.classList.remove('popup_opened');
 };
 
-const handleFormSubmit =
-    function handleFormSubmit(evt) {
-        evt.preventDefault();
-        nameTitle.textContent = nameInput.value;
-        jobSubtitle.textContent = jobInput.value;
-        closePopup();
-    }
+const handleFormSubmit = function (evt) {
+    evt.preventDefault();
+    nameTitle.textContent = nameInput.value;
+    jobSubtitle.textContent = jobInput.value;
+    closePopup();
+}
 
 popupOpenBottonElement.addEventListener('click', openPopup);
 popupCloseBottonElement.addEventListener('click', closePopup);
