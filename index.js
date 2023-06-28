712991aab315d96c5c17198fbba1a9952d76ac6d
Artem Dubravtsev <a.dubravtsev@yandex.ru>
2023-06-28 15:04:29 +0300

    Классы готовы

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-import initialCards from './scripts/utils/constants.js';
 import Card from './scripts/components/Card.js';
 import FormValidator from './scripts/components/FormValidator.js';
 import PopupWithImage from './scripts/components/PopupWithImage.js';
 import Section from './scripts/components/Section.js';
 import UserInfo from './scripts/components/UserInfo.js';
 import PopupWithForm from './scripts/components/PopupWithForm.js';
+import {
+    initialCards,
+    validationConfig,
+    formPopupSelector,
+    formPopupAddSelector,
+    popupOpenBottonElement,
+    popupAddOpenButton,
+    templateSelector,
+    popupEditProfileSelector,
+    popupAddCardSelector,
+    popupImageSelector,
+    elementSelector,
+    profileInfo
+} from './scripts/utils/constants.js';
 
-// Объект валидации
-const validationConfig = {
-    inputSelector: '.popup__form-item',                  // все импуты
-    submitButtonSelector: '.popup__button-save',         // Кнопки Сохранить/Создать
-    errorSpanSelector: '.popup__error_type_',            // Спан с текстом ошибки импута
-    inactiveButtonClass: 'popup__button-save_invalid',   // Модификатор не активной кнопки 
-    inputErrorClass: 'popup__form-item_error',           // Модификатор Красной полосы импута 
-    errorClass: 'popup__error_active'                    // Модификатор видимости ошибки ввода текста
-};
-
-const popup = document.querySelector('.popup');
-const popupEditProfileSelector = '.popup_edit';
-const popupAddCardSelector = '.popup_add';
-
-const popupOpenBottonElement = document.querySelector('.profile__button-edit');
-const popupAddOpenButton = document.querySelector('.profile__button-add');
-
-const formPopupSelector = popup.querySelector('.popup__form');
-const formPopupAddSelector = document.querySelector('.popup__form_add');
-
-const popupImageSelector = '.popup_image';
 const popupImage = new PopupWithImage(popupImageSelector);
 popupImage.setEventListeners();
 
-const templateSelector = '#cardElementTemplate';
-const elementSelector = '.elements';
 const section = new Section({
     items: initialCards,
     renderer: (cardElement) => {
@@ -41,10 +31,6 @@ const section = new Section({
 }, elementSelector);
 section.addCard();
 
-const profileInfo = {
-    profileName: '.profile__title',
-    profileJob: '.profile__subtitle'
-}
 const userInfo = new UserInfo(profileInfo)
 
 const popupProfil = new PopupWithForm(popupEditProfileSelector, (evt) => {
@@ -61,6 +47,13 @@ const popupAddCard = new PopupWithForm(popupAddCardSelector, (evt) => {
 });
 popupAddCard.setEventListeners();
 
+// Запуск валидации
+const formDataValidation = new FormValidator(validationConfig, formPopupSelector);
+formDataValidation.enableValidation();
+
+const formAddCardValidation = new FormValidator(validationConfig, formPopupAddSelector);
+formAddCardValidation.enableValidation();
+
 // Слушатель попапа информаций
 popupOpenBottonElement.addEventListener('click', () => {
     formDataValidation.resetErrorOpenForm();
@@ -72,12 +65,4 @@ popupOpenBottonElement.addEventListener('click', () => {
 popupAddOpenButton.addEventListener('click', () => {
     formAddCardValidation.resetErrorOpenForm();
     popupAddCard.open();
-})
-
-// Запуск валидации
-const formDataValidation = new FormValidator(validationConfig, formPopupSelector);
-formDataValidation.enableValidation();
-
-const formAddCardValidation = new FormValidator(validationConfig, formPopupAddSelector);
-formAddCardValidation.enableValidation();
-
+});
--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -26,4 +26,46 @@ const initialCards = [
     }
 ];
 
-export default initialCards;
+// Объект валидации
+const validationConfig = {
+    inputSelector: '.popup__form-item',                  // все импуты
+    submitButtonSelector: '.popup__button-save',         // Кнопки Сохранить/Создать
+    errorSpanSelector: '.popup__error_type_',            // Спан с текстом ошибки импута
+    inactiveButtonClass: 'popup__button-save_invalid',   // Модификатор не активной кнопки 
+    inputErrorClass: 'popup__form-item_error',           // Модификатор Красной полосы импута 
+    errorClass: 'popup__error_active'                    // Модификатор видимости ошибки ввода текста
+};
+
+const popup = document.querySelector('.popup');
+const formPopupSelector = popup.querySelector('.popup__form');
+const formPopupAddSelector = document.querySelector('.popup__form_add');
+
+const popupOpenBottonElement = document.querySelector('.profile__button-edit');
+const popupAddOpenButton = document.querySelector('.profile__button-add');
+
+const templateSelector = '#cardElementTemplate';
+
+const popupEditProfileSelector = '.popup_edit';
+const popupAddCardSelector = '.popup_add';
+const popupImageSelector = '.popup_image';
+const elementSelector = '.elements';
+
+const profileInfo = {
+    profileName: '.profile__title',
+    profileJob: '.profile__subtitle'
+}
+
+export {
+    initialCards,
+    validationConfig,
+    formPopupSelector,
+    formPopupAddSelector,
+    popupOpenBottonElement,
+    popupAddOpenButton,
+    templateSelector,
+    popupEditProfileSelector,
+    popupAddCardSelector,
+    popupImageSelector,
+    elementSelector,
+    profileInfo
+};
